68ba87e5d6d3d69c929321cd408cbd264456f802
Abdul-Malik-spl <12345+username@users.noreply.github.com>
2026-02-11 17:44:02 +0530

    dashboard additional change

--- src/Components/Dashboard/QuestionCreate.jsx
+++ src/Components/Dashboard/QuestionCreate.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import './QuestionWrapper.css'
+import InputNLabel from '../Auth/InputNLabel'
 export const QuestionCreate = () => {
+
   return (
     <div className='questionWrapper'>
         <div className='qustionForm'>
 <p className='rule'>Maximum 10 Question</p>
+<p className='rule'>Enter Options use - </p>
+<div className='form-question'>
+    {array.map((char,ind)=>{
+        return <InputNLabel {...char}/>
+    })}
+</div>
+    <div style={{display:"flex",justifyContent:"center",marginTop:"20px"}}><button className='questionAddBtn'>Add</button></div>
+
         </div>
     </div>
   )
 }
 
+let array=[{containerCls:"questionInputContainer",
+ inpId:"questionInput",
+label:"Question",
+ inpType:"text",
+inpPlaceholder:"",
+inpCls:"optioninpcls",
+error:"",
+errormsg:""},
+{containerCls:"questionInputContainer",
+ inpId:"optionsInput",
+label:"Options",
+ inpType:"text",
+inpPlaceholder:"op1-op2-op3-op3",
+inpCls:"optioninpcls",
+error:"",
+errormsg:""}
+]
 
 // containerCls
 // inpId
